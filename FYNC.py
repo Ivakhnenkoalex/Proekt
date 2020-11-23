50d114d6dd2d861ae20340bbe2660a4d30c98e0c
Ivakhnenkoalex <71816968+Ivakhnenkoalex@users.noreply.github.com>
2020-11-23 11:25:04 +0300

    Update FYNC.py
    Стадия 2

--- FYNC.py
+++ FYNC.py
@@ -1,5 +1,5 @@
 def s(m):
-    m[0],m[1] = m[1],m[0]
+    m[0], m[1] = m[1], m[0]
     return m
 
 
@@ -13,3 +13,25 @@ def shiftr(lst, steps):
             lst.insert(0, lst.pop())
 
 
+def ft_len(str):
+    a = 0
+    for i in str:
+        a += 1
+    return a
+
+
+def ft_rev_list(mass):
+    a = []
+    d = ft_len(mass)
+    c = d
+    i = 0
+    d = d - 1
+    while d > i:
+        a.append(mass[d])
+        d = d - 1
+    a.append(mass[0])
+    i = 0
+    while c > i:
+        mass[i] = a[i]
+        i = i + 1
+    return mass
