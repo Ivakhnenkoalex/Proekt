626c1f5621329faf0b1801069e55a7308094b600
Ivakhnenkoalex <71816968+Ivakhnenkoalex@users.noreply.github.com>
2020-12-02 17:29:03 +0300

    Пользуйся

--- RRB.py
+++ RRB.py
@@ -1,7 +1,22 @@
-def RRb(lst, steps):
-    steps = -1
-    if steps < 0:
-        lst = lst[1:]+lst[:1]
-    else:
-        lst = lst[-1:]+lst[:-1]
-    return lst
+def ft_s(m):
+    m[0], m[1] = m[1], m[0]
+    return m
+
+
+def RRa(a, st=1):
+    a = []
+    d = ft_len(m)
+    if st == 1:
+        a.append(m[d-1])
+        i = 0
+        while i < ft_len(m)-1:
+            a.append(m[i])
+            i = i + 1
+        return a
+    if st == -1:
+        i = 1
+        while i < ft_len(m):
+            a.append(m[i])
+            i = i + 1
+        a.append(m[0])
+        return a
