5b19dc71bc627c2e162fb5c71ae31736b0bdadf9
Ivakhnenkoalex <71816968+Ivakhnenkoalex@users.noreply.github.com>
2020-12-02 16:56:12 +0300

    Okk

--- Ra.py
+++ Ra.py
@@ -1,9 +1,7 @@
-def Ra(a): ## lst - это или a или b,steps  = 1 если ra или rb,steps = -1 если rra или rrb
-    if 1 < 0:
-        1 = abs(1)
-        for i in range(1):
-            a.append(a.pop(0))
+def Ra(lst,steps): ## lst - это или a или b,steps  = 1 если ra или rb,steps = -1 если rra или rrb
+    steps = 1
+    if steps < 0:
+        lst = lst[1:]+lst[:1]
     else:
-        for i in range(1):
-            a.insert(0, a.pop())
-    return a
+        lst = lst[-1:]+lst[:-1]
+    return lst
