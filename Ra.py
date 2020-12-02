91cd4041b55bcb1ea23e4f01cdc9b84f67b0ac4a
Ivakhnenkoalex <71816968+Ivakhnenkoalex@users.noreply.github.com>
2020-12-02 16:57:34 +0300

    okk

--- Ra.py
+++ Ra.py
@@ -1,5 +1,4 @@
-def Ra(lst,steps): ## lst - это или a или b,steps  = 1 если ra или rb,steps = -1 если rra или rrb
-    steps = 1
+def Ra(lst,steps=1): ## lst - это или a или b,steps  = 1 если ra или rb,steps = -1 если rra или rrb
     if steps < 0:
         lst = lst[1:]+lst[:1]
     else:
